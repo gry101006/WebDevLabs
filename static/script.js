4bc241311be23cacfc013a2b3816d1a478d55a7e
gry101006 <gry101006@gmail.com>
2025-03-25 15:03:36 -0400

    Web Dev Lab 6

--- static/script.js
+++ static/script.js
@@ -1,3 +1,52 @@
+x=5
+y=7
+z=x+y
+console.log(z)
+A='Hello '
+B='world!'
+C=A+B
+console.log(C)
+
+function SumNPrint(x1, x2){
+    x3 = x1 + x2
+    console.log(x3)
+    return x3
+}
+
+SumNPrint(x, y)
+SumNPrint(A, B)
+
+if (C.length >= z){
+    console.log("good job!")
+} else {
+    console.log(z)
+}
+
+L1 = ["Watermelon", "Pineapple", "Pear", "Banana"]
+L2 = ["Apple", "Banana", "Kiwi", "Orange"]
+
+/* function findTheBanana(L){
+    for(i = 0; i < L.length; i++){
+        if (L[i] == "Banana"){
+            window.alert(`found the Banana in ${i}`);
+            return i
+        }
+    }
+}
+
+findTheBanana(L1)
+findTheBanana(L2) */
+
+/* function findTheBanana(item, index, arr){
+    if (item == "Banana"){
+            window.alert(`found the Banana in ` + index);
+        }
+}
+
+
+L1.forEach(findTheBanana)
+L2.forEach(findTheBanana) */
+
 function greetingFunc(){
     d = new Date();
     h = d.getHours();
@@ -12,8 +61,8 @@ function greetingFunc(){
     }
     document.getElementById("greeting").innerHTML = greeting + ", I am Gryphon."
 }
-
-if (window.location.pathname.includes("index.html") || window.location.pathname == "/WebDevLabs/"){
+console.log(window.location.pathname)
+if (window.location.pathname.includes("index.html") || window.location.pathname == "/"){
     greetingFunc()   
 }
 
@@ -21,6 +70,10 @@ function addYear(){
     d = new Date();
     y = d.getFullYear();
     document.getElementById("copyYear").innerText = `Copyright ${y}, Gryphon Chong`
+}
+
+function showList(){
+    document.getElementById('funList').style.display = "block" 
 }
 
 
@@ -37,6 +90,9 @@ $(document).ready(function(){
         $("#introReadMore").show();
         $("#introReadLess").hide();
     });
+    $('#hamburgerMenu').click(function(){
+        $("a.menu").toggle();
+    });
 });
 
 function checkForm(){
@@ -51,15 +107,15 @@ function checkForm(){
     
 }
 
-function getAdvice(){
+function highlightActive(){
+    var nav = document.querySelectorAll("a.menu")
+    for (let i = 0; i < nav.length; i++) {
+        var currentElem = nav[i]
 
-    fetch(`https://api.adviceslip.com/advice?nocache=${new Date().getTime()}`)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("adviceText").innerText = data.slip.advice;
-    })
-    .catch(error => {
-        console.error("Error fetching advice:", error);
-        document.getElementById("adviceText").innerText = "Oops! Something went wrong, please try again.";
-    });
+        if (String(window.location).includes(currentElem.getAttribute("href")) ){
+            currentElem.style.color = "white";
+            currentElem.style.animation = "colorShifting 1s linear infinite alternate";
+        }
+    }
 }
+
